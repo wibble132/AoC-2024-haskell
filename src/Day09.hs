68778d002a9b36148431a9883358b6ac2d2d9f10
wibble132 <wibble132@gmail.com>
2024-12-09 19:51:37 +0000

    Slight tidy Day 9

--- src/Day09.hs
+++ src/Day09.hs
@@ -1,16 +1,11 @@
 module Day09 (part1, part2, readInput1, readInput2) where
 
-import Control.Monad (liftM2)
 import Data.Bifunctor (bimap)
 import Data.Char (digitToInt)
 import Data.Foldable (foldl')
 import Data.List (find)
 import Data.Vector (Vector)
 import Data.Vector qualified as V
-
--- import Debug.Trace (trace)
-trace :: String -> a -> a
-trace _ a = a 
 
 part1 :: [Block] -> Integer
 part1 bs = result
@@ -51,26 +46,19 @@ data File = File {fId :: Int, fPos :: Int, fLength :: Int} deriving (Show)
 data Space = Space {sPos :: Int, sLength :: Int} deriving (Show)
 
 readInput2 :: String -> ([File], [Space])
-readInput2 s = bimap makeFiles makeSpaces $ alternating withPositions
+readInput2 s = bimap makeFiles (map $ uncurry Space) $ alternating withPositions
   where
-    nums = map digitToInt s
-
-    positions :: [Int]
-    positions = getPositions nums -- reverse $ foldl' (\a b -> case a of [] -> [0]; (x : _) -> x + b : a) [] nums
     withPositions :: [(Int, Int)] -- [(position, length)]
-    withPositions = trace (show nums) $ trace (show positions) $ positions `zip` nums
+    withPositions = zip =<< getPositions $ map digitToInt s
 
     makeFiles :: [(Int, Int)] -> [File]
     makeFiles = zipWith (\i (pos, len) -> File i pos len) [0 ..]
-    makeSpaces :: [(Int, Int)] -> [Space]
-    makeSpaces = map (uncurry Space)
-
-getPositions :: [Int] -> [Int]
-getPositions = inner 0
-  where
-    inner :: Int -> [Int] -> [Int]
-    inner prev (x : xs) = prev : inner (prev + x) xs
-    inner prev [] = [prev]
+    getPositions :: [Int] -> [Int]
+    getPositions = inner 0
+      where
+        inner :: Int -> [Int] -> [Int]
+        inner prev (x : xs) = prev : inner (prev + x) xs
+        inner prev [] = [prev]
 
 alternating :: [a] -> ([a], [a])
 alternating (x : y : xs) = bimap (x :) (y :) $ alternating xs
@@ -78,27 +66,24 @@ alternating [x] = ([x], [])
 alternating [] = ([], [])
 
 order2 :: [File] -> [Space] -> [File]
-order2 files sps = newFiles
+order2 files sps = snd $ foldl' doStep (V.fromList sps, []) $ reverse files
   where
-    revFiles = reverse files
-
-    newFiles = snd $ foldl' doStep (V.fromList sps, []) revFiles
-
     doStep :: (Vector Space, [File]) -> File -> (Vector Space, [File])
     doStep (spaces, fs) nextFile = (nextSpaces, movedFile : fs)
       where
         (nextSpaces, movedFile) = step (squashSpaces spaces) nextFile
 
     step :: Vector Space -> File -> (Vector Space, File)
-    step !spaces file
-      | trace ("Step: " ++ show (V.map (liftM2 (,) sPos sLength) spaces)) False = undefined
+    step spaces file
       | Just (idx, space) <- fittingSpace =
-          trace
-            ("Moving " ++ show file ++ " to " ++ show idx ++ ", " ++ show space)
-            (spaces V.// [(idx, space `reduceBy` fLength file)], file `moveTo` sPos space)
+          ( -- Update the space we moved the file into (don't need to update the space it came from)
+            spaces V.// [(idx, space `reduceBy` fLength file)],
+            -- And update the file's position
+            file `moveTo` sPos space
+          )
       | otherwise = (spaces, file)
       where
-        -- The first space before this file that is larger than the file
+        -- The (Maybe) first space before this file that is larger than the file
         fittingSpace =
           find (\(_, s) -> sLength s >= fLength file)
             . zip [0 ..]
@@ -107,14 +92,13 @@ order2 files sps = newFiles
             $ spaces
 
 moveTo :: File -> Int -> File
-moveTo (File fId _ fLength) newPos
-  | trace ("Moving to " ++ show newPos) False = undefined
-  | otherwise = File fId newPos fLength
+moveTo (File fId _ fLength) newPos = File fId newPos fLength
 
 reduceBy :: Space -> Int -> Space
 reduceBy (Space sPos sLength) i = Space (sPos + i) (sLength - i)
 
 squashSpaces :: Vector Space -> Vector Space
+-- Very inefficient
 squashSpaces = V.fromList . inner . V.toList
   where
     inner :: [Space] -> [Space]
@@ -130,6 +114,5 @@ squashSpaces = V.fromList . inner . V.toList
 checksum2 :: [File] -> Int
 checksum2 = foldl' (\acc file -> acc + checksumFile file) 0
 
--- Can be improved, but this should work
 checksumFile :: File -> Int
 checksumFile file = fId file * sum (take (fLength file) [fPos file ..])
